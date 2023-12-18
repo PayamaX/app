d648b8d46771b80597ee92bc58ea5a9f4de89af0
Homayoun <eqpc@hbn.dev>
2023-12-18 11:15:32 +0330

    version updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "no1.PayamaX"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 1
+        versionName = "1.0.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
