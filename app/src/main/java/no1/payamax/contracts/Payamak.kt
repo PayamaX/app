3c7f2c505dda1327ce29b33e83dcf579760b877a
Homayoun (Ubuntu) <fj16@hbn.dev>
2024-01-02 20:46:38 +0100

    share added

--- app/src/main/java/no1/payamax/contracts/Payamak.kt
+++ app/src/main/java/no1/payamax/contracts/Payamak.kt
@@ -1,3 +1,11 @@
 package no1.payamax.contracts
 
-data class Payamak(val received: Long, val origin: Origin, val body: String)
+data class Payamak(val received: Long, val origin: Origin, val body: String) {
+    fun dump(indent: String): String {
+        val output = StringBuffer()
+        output.append("${indent}received: $received\n")
+        output.append("${indent}origin: ${origin.dump("$indent\t")}\n")
+        output.append("${indent}body:\n------------\n$body\n------------\n")
+        return output.toString()
+    }
+}
--- app/src/main/java/no1/payamax/contracts/Usability.kt
+++ app/src/main/java/no1/payamax/contracts/Usability.kt
@@ -6,4 +6,15 @@ data class Usability(
     val clazz: UsabilityClass,
     val rate: UsabilityRate,
     val processResults: List<ProcessResult>
-)
+) {
+    fun dump(indent: String): String {
+        val output = StringBuffer()
+        output.append("${indent}class: $clazz\n")
+        output.append("${indent}rate: ${rate.probability}\n")
+        for (pr in processResults) {
+            output.append(pr.dump("$indent\t"))
+            output.append("\n")
+        }
+        return output.toString()
+    }
+}
