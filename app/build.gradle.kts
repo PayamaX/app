744b58a8f002744bacc30306ddde8ba8e0df65c3
Homayoun (Ubuntu) <fj16@hbn.dev>
2024-01-04 19:38:22 +0100

    share added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "no1.PayamaX"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.0.0.7"
+        versionCode = 8
+        versionName = "1.0.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
